ec88ce1844ddd10cefd76b1e838af88f707a2248
Mathis Biabiany <mathis@resn.co.nz>
2018-01-17 15:28:44 +0100

    Setting up cursor view

---
+++ src/views/desktop/Cursor/index.js
@@ -0,0 +1,40 @@
+import createDOM from 'utils/dom/createDOM';
+import { autobind } from 'core-decorators';
+import { visible, toggle, opened, focused, selected } from 'core/decorators';
+import template from './cursor.tpl.html';
+import './cursor.scss';
+
+
+@visible()
+@toggle('slided', 'slide', 'unslide')
+@opened
+@focused
+@selected
+export default class CursorView {
+
+  // Setup ---------------------------------------------------------------------
+
+  constructor(options) {
+
+    this.el = options.parent.appendChild(
+      createDOM(template()),
+    );
+
+    this._setupEvents();
+  }
+
+  _setupEvents() {}
+
+  // State ---------------------------------------------------------------------
+
+  show({ delay = 0 } = {}) {
+    this.el.style.display = 'block';
+  }
+
+  hide({ delay = 0 } = {}) {
+    this._homeMenu.hide({ delay });
+  }
+
+  // Events --------------------------------------------------------------------
+
+}
